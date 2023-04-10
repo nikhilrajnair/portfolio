de1f53b4464254b719380ce3da3e55cad13b8ea4
Nikhil Raj A <nikhilrajnair.a@gmail.com>
2023-04-10 14:14:45 +0200

    fix: ui layout alignment

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -19,6 +19,10 @@ const PORTFOLIO_LIST: PortfolioList[] = [
   { id: 1, title: 'Velocity LLC', link: 'https://vintllc.com' },
   { id: 2, title: 'E.D. Marshall Jewelers', link: 'https://www.truventor.ai' },
   { id: 3, title: 'Truventor AI', link: 'https://www.truventor.ai' },
-  { id: 3, title: 'Wellthy Therapeutics', link: 'https://www.wellthytherapeutics.com/' },
+  { id: 4, title: 'Wellthy Therapeutics', link: 'https://www.wellthytherapeutics.com/' },
+  { id: 7, title: 'Ozo', link: 'https://ozo.club/' },
+  { id: 5, title: 'Spiceland Travel', link: 'http://spiceland.travel/' },
+  { id: 6, title: 'La Salette India', link: 'http://lasaletteindia.com/' },
+  { id: 7, title: 'La Salette India', link: 'https://www.muthoothealthcare.com/kozhenchery/' },
 
 ];
