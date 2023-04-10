82f55d52844238c3441d65042e60d37e38d0bc0c
Nikhil Raj A <nikhilrajnair.a@gmail.com>
2023-04-10 12:32:29 +0200

    feat: porfolio as dynamic list

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-
+  public portfolioList: PortfolioList[] = PORTFOLIO_LIST;
 }
+
+export interface PortfolioList {
+  id: number;
+  title: string;
+  link: string;
+}
+
+const PORTFOLIO_LIST: PortfolioList[] = [
+  { id: 1, title: 'Velocity LLC', link: 'https://vintllc.com' },
+  { id: 2, title: 'E.D. Marshall Jewelers', link: 'https://www.truventor.ai' },
+  { id: 3, title: 'Truventor AI', link: 'https://www.truventor.ai' },
+  { id: 3, title: 'Wellthy Therapeutics', link: 'https://www.wellthytherapeutics.com/' },
+
+];
